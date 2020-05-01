57e1cdeb11949ac0f7b4ab2dfee80581d0ee0e41
Michał Chęciński <michal.checinski@outlook.com>
2020-05-01 10:46:16 +0200

    fix function deleting backups

--- DeleteServerBackups/run.ps1
+++ DeleteServerBackups/run.ps1
@@ -25,27 +25,30 @@ $containers = Get-AzStorageContainer -Prefix "backup-" -Context $stContext
 
 Write-Host ($containers | Select-Object Name)
 
-if ($containers.count -gt 6 ) {
+if ($containers.count -gt 5 ) {
     $message = ''
 
     foreach ($container in $containers) {
+        $containers = Get-AzStorageContainer -Prefix "backup-" -Context $stContext
+        if ($containers.count -gt 5 ) {
 
-        $dirName = $container.Name.Replace("backup-", "")
+            $dirName = $container.Name.Replace("backup-", "")
 
-        # Variable for directory date
-        [datetime]$dirDate = New-Object DateTime
+            # Variable for directory date
+            [datetime]$dirDate = New-Object DateTime
 
-        # Check that directory name could be parsed to DateTime
-        if ([DateTime]::TryParseExact($dirName, "yyyy-MM-dd",
-                [System.Globalization.CultureInfo]::InvariantCulture,
-                [System.Globalization.DateTimeStyles]::None,
-                [ref]$dirDate)) {
-            if (([DateTime]::Today - $dirDate).TotalDays -ge 14) {
-                $containerName = $container.Name
-                $message = $message + "$containerName\n"
+            # Check that directory name could be parsed to DateTime
+            if ([DateTime]::TryParseExact($dirName, "yyyy-MM-dd",
+                    [System.Globalization.CultureInfo]::InvariantCulture,
+                    [System.Globalization.DateTimeStyles]::None,
+                    [ref]$dirDate)) {
+                if (([DateTime]::Today - $dirDate).TotalDays -ge 14) {
+                    $containerName = $container.Name
+                    $message = $message + "$containerName\n"
 
-                Write-Host "Removing $containerName"
-                Remove-AzStorageContainer -Name $containerName -StorageAccountKey $stKey -Force
+                    Write-Host "Removing $containerName"
+                    Remove-AzStorageContainer -Name $containerName -StorageAccountKey $stKey -Force
+                }
             }
         }
     }
