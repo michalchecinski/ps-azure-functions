c955fd43bc2df337069f08cb890de52938c16655
Michał Chęciński <michal.checinski@outlook.com>
2020-07-31 22:35:13 +0200

    Delete logging

--- DeleteServerBackups/run.ps1
+++ DeleteServerBackups/run.ps1
@@ -1,33 +1,12 @@
 # Input bindings are passed in via param block.
 param($Timer)
 
-function SendSlackMessage {
-    param (
-        [string]$message
-    )
-
-    $SlackChannelUri = $env:SlackWebhookUri
-
-    $BodyTemplate = @"
-        {
-            "text": "*Removed backups:* \n $message \nTime: $(Get-Date).",
-        }
-"@
-
-    Invoke-RestMethod -uri $SlackChannelUri -Method Post -body $BodyTemplate -ContentType 'application/json'
-
-}
-
 $stKey = $env:LocalServerBackupStorageKey
 $stContext = New-AzStorageContext -StorageAccountName "hasslocalserverbackup" -StorageAccountKey $stKey
 
 $containers = Get-AzStorageContainer -Prefix "backup-" -Context $stContext
 
-Write-Host ($containers | Select-Object Name)
-
 if ($containers.count -gt 5 ) {
-    $message = ''
-
     foreach ($container in $containers) {
         $containers = Get-AzStorageContainer -Prefix "backup-" -Context $stContext
         if ($containers.count -gt 5 ) {
@@ -44,17 +23,12 @@ if ($containers.count -gt 5 ) {
                     [ref]$dirDate)) {
                 if (([DateTime]::Today - $dirDate).TotalDays -gt 14) {
                     $containerName = $container.Name
-                    $message = $message + "$containerName\n"
 
                     Write-Host "Removing $containerName"
                     Remove-AzStorageContainer -Name $containerName -Context $stContext -Force
                 }
             }
         }
-    }
-
-    if ($message) {
-        SendSlackMessage $message
     }
 }
 
